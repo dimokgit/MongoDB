086598daa041485b46943569b541d38f76b91b7c
Dmitri Lapchine <dimokhot@hotmail.com>
2018-05-13 13:37:22 -0400

    Nuget bump

--- Mongo.Console/Program.cs
+++ Mongo.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using HedgeHog.MongoDB;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,16 @@ namespace Mongo {
   class Program {
     static void Main(string[] args) {
       using(var db = new ForexDbContext()) {
-        db.ToDos.Add(new ToDo { What = "Debug", When = DateTime.UtcNow});
+        var trades = db.Trades.ToArray();
+        var openDate = DateTime.Parse("01/04/2018 9:35:48");
+        db.Trades.Add(new Trade("DImok", 1, 2, 10, 20, 100, openDate, openDate.AddSeconds(60 * 10 + 19),48));
+        db.SaveChanges();
+        foreach(var o in db.Trades) {
+          Console.WriteLine(new { o.Symbol, o.OpenPrice, o.ClosePrice, o.TimeOpen, o.TimeClose,o.PL } + "");
+        }
+        Console.ReadKey();
+        return;
+        db.ToDos.Add(new ToDo { What = "Debug", When = DateTime.UtcNow });
         try {
           db.SaveChanges();
           db.ToDos.First(x => x.What == "Debug").What = "Test";
@@ -22,7 +32,7 @@ namespace Mongo {
           db.SaveChanges();
 
           toDos = db.ToDos.ToArray();
-          toDos.First(x => x.What == "Debug").What = "Test "+DateTime.Now.Second;
+          toDos.First(x => x.What == "Debug").What = "Test " + DateTime.Now.Second;
           db.SaveChanges();
           foreach(var td in db.ToDos) {
             Console.WriteLine($"{td.When.ToLocalTime().TimeOfDay}: {td.What}");
@@ -39,7 +49,12 @@ namespace Mongo {
           Console.WriteLine(new { o.Pair, o.MMRLong } + "");
         }
 
-        Console.ReadKey();
+        //var tms = db.TradingMacroSettings.ToArray();
+        //db.TradingMacroSettings.Add(new TradingMacroSettings { Pair = "Dimok", PairIndex = 1 });
+        //db.SaveChanges();
+        //foreach(var o in tms) {
+        //  Console.WriteLine(new { o.Pair, o.PairIndex } + "");
+        //}
       }
     }
   }
